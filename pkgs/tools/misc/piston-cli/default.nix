ef2ae9afcf328fe676e58bf6395539d811446f38
Ethan Edwards <ethan@ethancedwards.com>
2021-10-23 11:41:39 -0400

    for the time being

--- pkgs/tools/misc/piston-cli/default.nix
+++ pkgs/tools/misc/piston-cli/default.nix
@@ -2,14 +2,14 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "piston-cli";
-  version = "1.4.3";
+  version = "1.5.0";
 
   src = python3Packages.fetchPypi {
     inherit pname version;
-    sha256 = "qvDGVJcaMXUajdUQWl4W1dost8k0PsS9XX/o8uQrtfY=";
+    sha256 = "YzQo22/63IJ78Y7pqk7W1galI+HNk7gIodiDlHwNllY=";
   };
 
-  propagatedBuildInputs = with python3Packages; [ rich prompt-toolkit requests pygments pyyaml more-itertools ];
+  propagatedBuildInputs = with python3Packages; [ coloredlogs requests-cache rich prompt-toolkit requests pygments pyyaml more-itertools ];
 
   checkPhase = ''
     $out/bin/piston --help > /dev/null
