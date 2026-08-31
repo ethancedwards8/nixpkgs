150a85d2c6a41b82025f89d87fb843b11fc4b01a
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-08-30 21:55:01 -0400

    worktrunk: use installAgentSkills
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>
    (cherry picked from commit 59369f752ceaaaece2acdd30f2d1a1160479c69e)

--- pkgs/by-name/wo/worktrunk/package.nix
+++ pkgs/by-name/wo/worktrunk/package.nix
@@ -5,7 +5,9 @@
   rustPlatform,
   gitMinimal,
   installShellFiles,
+  installAgentSkills,
   versionCheckHook,
+  writableTmpDirAsHomeHook,
   nix-update-script,
 }:
 
@@ -30,16 +32,24 @@ rustPlatform.buildRustPackage (finalAttrs: {
 
   nativeBuildInputs = [
     installShellFiles
+    installAgentSkills
+    writableTmpDirAsHomeHook
   ];
 
-  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-    # wt reads config from $HOME; provide a throwaway dir so it doesn't fail.
-    export HOME="$(mktemp -d)"
+  dontInstallAgentSkills = true;
 
+  postInstall = ''
+    installSkill skills/worktrunk worktrunk
+    installSkill skills/wt-switch-create worktrunk
+  ''
+  + lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+
+    # wt reads config from $HOME; provide a throwaway dir so it doesn't fail.
     installShellCompletion --cmd wt \
       --bash <($out/bin/wt config shell completions bash) \
       --fish <($out/bin/wt config shell completions fish) \
-      --zsh  <($out/bin/wt config shell completions zsh)
+      --zsh  <($out/bin/wt config shell completions zsh) \
+      --nushell <($out/bin/wt config shell completions nu)
   '';
 
   nativeCheckInputs = [ gitMinimal ];
