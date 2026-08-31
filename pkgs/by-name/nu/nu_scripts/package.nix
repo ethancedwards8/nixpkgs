b3b1874497d4db6e3c20920577f7119f177fca63
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-08-30 21:11:13 -0400

    nu_scripts: use installAgentSkills
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>
    (cherry picked from commit 5c16698680e302fce32a0b28a6b11df0cc581081)

--- pkgs/by-name/nu/nu_scripts/package.nix
+++ pkgs/by-name/nu/nu_scripts/package.nix
@@ -2,6 +2,7 @@
   lib,
   stdenvNoCC,
   fetchFromGitHub,
+  installAgentSkills,
   unstableGitUpdater,
 }:
 
@@ -16,12 +17,15 @@ stdenvNoCC.mkDerivation {
     hash = "sha256-JUsFHsacED+7eTLAlfBnF9vasIHWIL/POJMCPbC9Baw=";
   };
 
+  nativeBuildInputs = [ installAgentSkills ];
+
   installPhase = ''
     runHook preInstall
 
     mkdir -p $out/share/nu_scripts
     mv ./* $out/share/nu_scripts
     rm -r $out/share/nu_scripts/themes/screenshots
+    rm -r $out/share/nu_scripts/skills/ # installAgentSkills installs these in the correct directory
 
     runHook postInstall
   '';
